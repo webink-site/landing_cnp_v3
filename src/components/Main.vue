b0058102760d61aa332461227c3234eb8b19cd3d
Константин Тимофеев <kt@webink.site>
2021-10-29 20:59:55 +0300

    видосы

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,12 @@
 <template>
+<div>
+
     <section id="main">
+        <div class="bgfon">
+            <iframe src="https://player.vimeo.com/video/639909245?h=00bc65a83f&loop=1&autopause=0&amp;byline=false&amp;controls=false&amp;fun=false&amp;muted=true&amp;portrait=false&amp;title=false&amp;autoplay=true&amp;transparent=true&amp;gesture=media" allowtransparency allow="autoplay" class="bg-video__player hidden-lg hidden-md hidden-sm"></iframe>
+        </div>
+
+
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -13,36 +20,44 @@
                                 <button class="vio-btn" v-scroll-to="'#payment'">Записаться со скидкой</button>
                             </div>
                              <div class="col-lg-6">
-                                <div class="main-video">
+                                <div class="main-video hidden-xs hidden-sm">
                                     <iframe src='https://player.vimeo.com/video/639909245?h=00bc65a83f&loop=1&autopause=0&amp;byline=false&amp;controls=false&amp;fun=false&amp;muted=true&amp;portrait=false&amp;title=false&amp;autoplay=true&amp;transparent=true&amp;gesture=media' frameborder='0' webkitAllowFullScreen="false" mozallowfullscreen="false" allowFullScreen="false"></iframe>
                                 </div>
                             </div>
                             <div class="col-lg-3">
                                 <div class="icon-box">
                                     <img src="../assets/img/mi1.svg" alt="">
-                                    <h3>Начало обучения</h3>
-                                    <p>27 октября</p>
+                                    <div>
+                                        <h3>Начало обучения</h3>
+                                        <p>{{getTomorrow}}</p>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-lg-3">
                                 <div class="icon-box">
                                     <img src="../assets/img/mi2.svg" alt="">
-                                    <h3>Длительность</h3>
-                                    <p>4 недели</p>
+                                    <div>
+                                        <h3>Длительность</h3>
+                                        <p>4 недели</p>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-lg-3">
                                 <div class="icon-box">
                                     <img src="../assets/img/mi3.svg" alt="">
-                                    <h3>Практика</h3>
-                                    <p>35 лекций </p>
+                                    <div>
+                                        <h3>Практика</h3>
+                                        <p>35 лекций </p>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-lg-3">
                                 <div class="icon-box">
                                     <img src="../assets/img/mi4.svg" alt="">
-                                    <h3>Что нужно</h3>
-                                    <p>Для прохождения нужен только телефон</p>
+                                    <div>
+                                        <h3>Что нужно</h3>
+                                        <p>Для прохождения нужен только телефон</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -51,4 +66,25 @@
             </div>
         </div>
     </section>
+</div>
 </template>
+
+
+<script>
+
+export default{
+    data(){
+       return{
+            curentDate: new Date() 
+       }
+    },
+    computed :{
+        getTomorrow(){
+            var today = new Date();
+            var tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+            console.log()
+            return tomorrow.toLocaleString('ru', { month: 'long', day: 'numeric'})
+        }
+    }
+}
+</script>
--- src/components/MyWorks.vue
+++ src/components/MyWorks.vue
@@ -1,31 +1,42 @@
 <template>
-    <section id="myworks">
+    <section id="myworks" v-if="videos.length">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <h2>Работы</h2>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-6" v-for="item in videos" >
                     <div class="video-box">
-                        <iframe src="https://player.vimeo.com/video/362562070" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%"></iframe>
+                        <iframe :src="`https://player.vimeo.com/video/${item[0].id}`" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%"></iframe>
                     </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="video-box">
-                        <iframe src="https://player.vimeo.com/video/298837516" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%"></iframe>
+                    <div class="video-title">
+                        {{item[0].title}}
                     </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="video-box">
-                        <iframe src="https://player.vimeo.com/video/232135457" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%"></iframe>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="video-box">
-                        <iframe src="https://player.vimeo.com/video/232435796" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%"></iframe>
-                    </div>
+                    <p class="small-grey">
+                        New
+                    </p>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+        return{
+            videos: []
+        }
+    },
+    created(){
+            axios
+             .get('https://nikitapugachev.ru/wp-json/np/v1/get/videomain')
+             .then(res =>{
+                console.log(res.data.data)
+                this.videos = res.data.data
+            })
+    }
+}
+</script>
