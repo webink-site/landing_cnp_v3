5baa7da3b5a0caa6e90a1a58e6b4cd0474ebc9b4
Константин Тимофеев <kt@webink.site>
2021-11-10 14:34:50 +0300

    freelesson

--- src/components/FreeLesson.vue
+++ src/components/FreeLesson.vue
@@ -5,7 +5,7 @@
                 <div class="answer-box text-center">
                     <h2>Бесплатный урок</h2>
                     
-                    <a href="">
+                    <a href="https://t.me/nikitapugachevbot?start=freevideo">
                         <button class="white-btn">Пройти бесплатный урок</button>
                     </a>
                 </div>
