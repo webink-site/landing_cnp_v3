2cefd2899c585767011a309cb51e9cefbb103661
Константин Тимофеев <kt@webink.site>
2021-11-10 20:22:58 +0300

    scroll to payment

--- src/router/index.js
+++ src/router/index.js
@@ -6,8 +6,17 @@ import store from '../store'
 const router = new VueRouter({
     routes,
     mode: 'history',
-    scrollBehavior(to, from, savedPosition) {
-        return { x: 0, y: 0 }
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+          return savedPosition
+        }else if (to.hash) {
+          return {
+            selector: to.hash
+            // , offset: { x: 0, y: 10 }
+          }
+        }else {
+          return { x: 0, y: 0 }
+        }
     }
 });
 
